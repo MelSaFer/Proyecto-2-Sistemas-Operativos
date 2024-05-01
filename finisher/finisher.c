c33dea4b975c554b616692c10cee97eb00fb20a0
fiozelaya <fiorella.zelaya1024@estudiantec.cr>
2024-04-30 23:01:11 -0600

    Working on starter: Creating shared memory and attaching it

--- finisher/finisher.c
+++ finisher/finisher.c
@@ -3,9 +3,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int finishEnvironment(key_t processSharedMemoryKey) {
+int finishEnvironment(key_t processSharedMemoryKey, key_t controlSharedMemory) {
     // Obtener el identificador de la memoria compartida
     int shmid = shmget(processSharedMemoryKey, 0, 0644);
+    if (shmid == -1) {
+        perror("Error obtaining the shared memory ID");
+        exit(1);
+    }
+
+    // Destruir la memoria compartida
+    if (shmctl(shmid, IPC_RMID, NULL) == -1) {
+        perror("Error destroying the shared memory");
+        exit(1);
+    }
+    printf("\nShared memory destroyed!!!\n");
+
+    // Obtener el identificador de la memoria compartida
+    shmid = shmget(controlSharedMemory, 0, 0644);
     if (shmid == -1) {
         perror("Error obtaining the shared memory ID");
         exit(1);
@@ -24,6 +38,7 @@ int finishEnvironment(key_t processSharedMemoryKey) {
 
 int main() {
     key_t processSharedMemoryKey = 22337049;
-    finishEnvironment(processSharedMemoryKey);
+    key_t controlSharedMemory = 22337050;
+    finishEnvironment(processSharedMemoryKey, controlSharedMemory);
     return 0;
 }
--- sharedMem.h
+++ sharedMem.h
@@ -5,13 +5,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include "memPartition.h"
+//#include "memPartition.h"
+#include "thread.h"
 
 #define MAX_LINES 100
 
 struct SHAREDMEM{
     int lines;
-    struct MEMPARTITION partitions[MAX_LINES];
+    //struct MEMPARTITION partitions[MAX_LINES];
+    struct THREAD partitions[MAX_LINES];
 };
 
 // State = 0;
